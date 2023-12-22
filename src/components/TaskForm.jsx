3668da1c2956007fea356cd4671d24dbdbab567d
Sahil <garg07825@gmail.com>
2023-12-23 03:43:33 +0530

    created a taskForm component

---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TaskForm = () => {
+  return (
+    <form action="">
+      <button>+</button>
+      <input type="text" placeholder="Your next Task" />
+    </form>
+  );
+};
+
+export default TaskForm;
