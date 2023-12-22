480cf5ce1b062f5de26cb471e3ba34776570ea23
Sahil <garg07825@gmail.com>
2023-12-23 03:48:45 +0530

    created second component Task

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import './App.css'
-import TaskForm from './components/TaskForm'
+import "./App.css";
+import TaskForm from "./components/TaskForm";
+import Task from "./components/task";
 
 function App() {
   return (
-  <div>
-  <TaskForm />
-  </div>
-  )
+    <div>
+      <TaskForm />
+      <Task />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TaskForm = () => {
+  const [taskName, setTaskName] = useState("");
   return (
     <form action="">
       <button>+</button>
-      <input type="text" placeholder="Your next Task" />
+      <input
+        type="text"
+        value={taskName}
+        onChange={(e) => setTaskName(e.target.value)}
+        placeholder="Your next Task"
+      />
     </form>
   );
 };
