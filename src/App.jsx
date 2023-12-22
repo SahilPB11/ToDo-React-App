cce8dc3e2768fdd23e192c8bc53fe73608f07d01
Sahil <garg07825@gmail.com>
2023-12-23 04:45:57 +0530

    i am set and get the tasks from localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import TaskForm from "./components/TaskForm";
 import Task from "./components/task";
 
 function App() {
   const [tasks, setTasks] = useState([]);
+  // here i am saving the task in localstorage whenever the changes will happen
+  useEffect(() => {
+    localStorage.setItem("task", JSON.stringify(tasks));
+  }, [tasks]);
+
+  // after refreh we are checking if tasks is present in localstorage show it
+  useEffect(() => {
+    setTasks(JSON.parse(localStorage.getItem(tasks)));
+  }, []);
 
   // this function for adding a new task
   function addTask(name) {
